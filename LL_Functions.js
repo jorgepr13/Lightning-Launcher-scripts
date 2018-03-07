aea99ff2bbac574943e07487d33193a249dcf3c7
Jorge <16358444+jorgepr13@users.noreply.github.com>
2018-03-08 04:10:01 +0800

    started the creation of the functions

--- LL_Functions.js
+++ LL_Functions.js
@@ -169,6 +169,243 @@ the image can be obtained and applied to another item
 0x00000000 //transparent black
 */
 
+
+/*
+addCustomView (float x, float y)
+Create a new custom view using default values.
+
+addFolder (String label, float x, float y)
+Create a new empty folder with the default icon and size.
+Parameters
+label	folder name
+x	absolute position (rounded to nearest cell when attached to grid)
+y	absolute position (rounded to nearest cell when attached to grid)
+Returns the new folder
+
+public PageIndicator addPageIndicator (float x, float y)
+Create a new page indicator using default values.
+
+addPanel (float x, float y, float width, float height)
+Create a new empty panel. All coordinates are in pixels and rounded to the nearest cell when the item is attached to the grid.
+Returns the new panel
+
+addShortcut (String label, Intent intent, float x, float y)
+Create a new shortcut with the default icon and size.
+Parameters
+label	shortcut text
+intent	shortcut action, see http://developer.android.com/reference/android/content/Intent.html
+x	absolute position (rounded to nearest cell when attached to grid)
+y	absolute position (rounded to nearest cell when attached to grid)
+Returns the new shortcut
+
+addStopPoint (float x, float y)
+Create a new stop point with the default configuration.
+Parameters
+x	absolute position (rounded to nearest cell when attached to grid)
+y	absolute position (rounded to nearest cell when attached to grid)
+Returns the new stop point
+
+getType ()
+Used to identify the kind of this container.
+Returns 'Desktop', or 'Container' for any other type of container (folder, panel).
+
+getType ()
+Returns the item type has a text
+Returns one of: Shortcut, Folder, Panel, Widget, StopPoint, DynamicText, Unlocker, PageIndicator or Unknown
+
+public Container getParent ()
+Returns the parent container for this container, or null if this container is a desktop.
+
+public Container getParent ()
+Returns the container containing this item.
+
+
+*/
+
+//get current container
+function makeShortcut(container, name) {
+  if (emptyVariable(container) || emptyVariable(name)) {showToast("makeShortcut Error: No container or name were set."); return;}
+  item = container.getItemByName(name);
+  if (item == null) {
+    item = container.addShortcut("", intent, 0, 0); //label, intent, x, y
+    item.setName(name);
+  }
+  return item;
+}
+
+function getName(item) {if (emptyVariable(item)) {return;}
+  item.getName();
+}
+function setName(item, name) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(name)) {name = "";}
+  item.setName(name);
+}
+
+function showLabel(item) {if (emptyVariable(item)) {return;}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setBoolean("s.labelVisibility", true).commit();}
+}
+function hideLabel(item) {if (emptyVariable(item)) {return;}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setBoolean("s.labelVisibility", false).commit();}
+}
+function getLabel(item) {if (emptyVariable(item)) {return;}
+  var label = ""; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {label = item.getLabel();}
+  return label;
+}
+function setLabel(item, label) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(label)) {label = "";}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.setLabel(label, true);}
+}
+function getLabelSize(item) {if (emptyVariable(item)) {return;}
+  var size = 0; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {size = item.getProperties().getFloat("s.labelFontSize");}
+  return size;
+}
+function setLabelSize(item, size) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(size)) {size = 10;}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setFloat("s.labelFontSize", size).commit();}
+}
+function getLabelStyles() {return ["NORMAL", "ITALIC", "BOLD", "BOLD_ITALIC"];}
+function getLabelStyle(item) {if (emptyVariable(item)) {return;}
+  var style = ""; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {style = item.getProperties().getFloat("s.labelFontSize");}
+  return style;
+}
+function setLabelStyle(item, style) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(style)) {style = "NORMAL";}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setFloat("s.labelFontSize", style).commit();}
+}
+function getLabelFont(item) {if (emptyVariable(item)) {return;}
+  var font = ""; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {font = item.getProperties().getFloat("s.labelFontTypeFace");}
+  return font;
+}
+function setLabelFont(item, font) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(font)) {font = "";}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setFloat("s.labelFontTypeFace", font).commit();}
+}
+function getLabelColor(item) {if (emptyVariable(item)) {return;}
+  var color = 0xffffffff; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {color = item.getProperties().getFloat("s.labelFontColor");}
+  return color;
+}
+function setLabelColor(item, color) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(color)) {color = 0xffffffff;}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setFloat("s.labelFontColor", color).commit();}
+}
+function getLabelColorFocused(item) {if (emptyVariable(item)) {return;}
+  var color = 0xffffffff; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {color = item.getProperties().getFloat("s.focusColorLabel");}
+  return color;
+}
+function setLabelColorFocused(item, color) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(color)) {color = 0xffffffff;}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setFloat("s.focusColorLabel", color).commit();}
+}
+function getLabelColorSelected(item) {if (emptyVariable(item)) {return;}
+  var color = 0xffffffff; var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {color = item.getProperties().getFloat("s.selectionColorLabel");}
+  return color;
+}
+function setLabelColorSelected(item, color) {if (emptyVariable(item)) {return;}
+  if (emptyVariable(color)) {color = 0xffffffff;}
+  var type = item.getType().toLowerCase();
+  if (type == "folder" || type == "shortcut") {item.getProperties().edit().setFloat("s.selectionColorLabel", color).commit();}
+}
+
+
+
+
+
+LLShortcut.prototype.setProperties = function() {
+  if (emptyVariable(this.item)) {this.makeShortcut();}
+  if (emptyVariable(this.item)) {showToast("set Prop Error: Unable to find the item.", true); return;}
+  this.setItemProperties();
+  if (emptyVariable(this.label)) {this.item.setLabel(this.pkg_label, true);}
+  //this.item.setIntent(this.intent);
+  //this.item.setDefaultIcon(this.icon);
+
+  var ed = this.item.getProperties().edit();
+  //ed.setBoolean("s.iconVisibility", 	     false);
+  ed.setBoolean("s.iconVisibility",       true);
+  ed.commit();
+}
+
+
+LLShortcut.prototype.getLabel = function() {return this.label;}
+LLShortcut.prototype.setLabel = function(label) {if (!emptyVariable(label)) {this.label = label;}}
+
+LLShortcut.prototype.getLabelSize = function() {return this.label_size;}
+LLShortcut.prototype.setLabelSize = function(size) {if (!emptyVariable(size) || !isNaN(size)) {this.label_size = size;}}
+
+LLShortcut.prototype.getPkg = function() {return this.pkg;}
+LLShortcut.prototype.setPkg = function(pkg) {if (!emptyVariable(pkg)) {this.pkg = pkg;}}
+
+LLShortcut.prototype.getPkgLabel = function() {return this.pkg_label;}
+LLShortcut.prototype.setPkgLabel = function(label) {if (!emptyVariable(label)) {this.pkg_label = label;}}
+
+LLShortcut.prototype.getIntent = function() {return this.intent;}
+LLShortcut.prototype.setIntent = function(intent) {if (!emptyVariable(intent)) {this.intent = intent;}}
+
+LLShortcut.prototype.getIcon = function() {return this.icon;}
+LLShortcut.prototype.setIcon = function(icon) {if (!emptyVariable(icon)) {this.icon = icon;}}
+
+LLShortcut.prototype.create = function() {this.getAppInfo(); this.makeShortcut(); this.setProperties();}
+//this.item = makeShortcut(this.container, this.name, this.label, this.intent);}
+//shortcut_make_mod(null,this.name,this.size,this.label,this.pkg_label,this.pkg,this.intent);
+
+LLShortcut.prototype.getAppInfo = function() {
+  //if (emptyVariable(this.pkg) && emptyVariable(this.pkg_label)) {showToast("getAppInfo  Error: Not enough data to find the app.", true); return;}
+  if (emptyVariable(this.pkg) && emptyVariable(this.pkg_label)) {return;}
+  if (this.container.getItemByName(this.name) != null) {return;}
+  var myIntent = null;
+  //var pm = context.getPackageManager();
+  if (!emptyVariable(this.pkg)) {
+    try {myIntent = pm.getLaunchIntentForPackage(this.pkg);} catch (e) {myIntent = null;}
+  }
+  if (!emptyVariable(this.pkg_label) && myIntent == null) {
+    var labelLow = this.pkg_label.toLowerCase();
+    var pkg_name = [];
+
+    var intent = new Intent(Intent.ACTION_MAIN);
+    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+    if (emptyVariable(apps)) {apps = pm.queryIntentActivities(intent, 0);}
+    var len = apps.size();
+    for (var j = 0; j < len; j++) {
+      var app = apps.get(j);
+      var name = app.activityInfo.name;
+      var pkg = app.activityInfo.packageName;
+      var lbl = app.loadLabel(pm).toLowerCase();
+      if (lbl == labelLow) {intent.setClassName(pkg, name); myIntent = intent; break;}
+      if (lbl.search(labelLow) != -1) {pkg_name.push([pkg, name]);}
+    }
+    if (myIntent == null && pkg_name.length > 0) {intent.setClassName(pkg_name[0][0], pkg_name[0][1]); myIntent = intent;}
+  }
+  if (myIntent != null) {
+    var app = pm.queryIntentActivities(myIntent, 0).get(0);
+    var bmp = pm.getActivityIcon(myIntent).getBitmap();
+    var img = Image.createImage(bmp.getWidth(), bmp.getHeight());
+    img.draw().drawBitmap(bmp, 0, 0, null);
+    this.intent = myIntent;
+    this.pkg_label = app.loadLabel(pm);
+    this.pkg = app.activityInfo.packageName;
+    this.icon = img;
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
 var apps;
 var pm = context.getPackageManager();
 
@@ -520,90 +757,6 @@ LLPanel.prototype.makePanel = function() {
 
 
 /*
-addCustomView (float x, float y)
-Create a new custom view using default values.
-
-addFolder (String label, float x, float y)
-Create a new empty folder with the default icon and size.
-Parameters
-label	folder name
-x	absolute position (rounded to nearest cell when attached to grid)
-y	absolute position (rounded to nearest cell when attached to grid)
-Returns the new folder
-
-public PageIndicator addPageIndicator (float x, float y)
-Create a new page indicator using default values.
-
-addPanel (float x, float y, float width, float height)
-Create a new empty panel. All coordinates are in pixels and rounded to the nearest cell when the item is attached to the grid.
-Returns the new panel
-
-addShortcut (String label, Intent intent, float x, float y)
-Create a new shortcut with the default icon and size.
-Parameters
-label	shortcut text
-intent	shortcut action, see http://developer.android.com/reference/android/content/Intent.html
-x	absolute position (rounded to nearest cell when attached to grid)
-y	absolute position (rounded to nearest cell when attached to grid)
-Returns the new shortcut
-
-addStopPoint (float x, float y)
-Create a new stop point with the default configuration.
-Parameters
-x	absolute position (rounded to nearest cell when attached to grid)
-y	absolute position (rounded to nearest cell when attached to grid)
-Returns the new stop point
-
-getType ()
-Used to identify the kind of this container.
-Returns 'Desktop', or 'Container' for any other type of container (folder, panel).
-
-getType ()
-Returns the item type has a text
-Returns one of: Shortcut, Folder, Panel, Widget, StopPoint, DynamicText, Unlocker, PageIndicator or Unknown
-
-public Container getParent ()
-Returns the parent container for this container, or null if this container is a desktop.
-
-public Container getParent ()
-Returns the container containing this item.
-
-
-*/
-
-
-
-//########################
-//########################
-//########################
-// find item > name, id, container
-//check container, from current desktop recursive skip container, check all desktops recursive skip prev desktop
-//check drawer
-//if not found, ask to create it
-//return array of items
-//will help at the creation, with badges positioning, removal
-
-//var soc = new LLPanel(); soc.setName("social_test"); soc.setPosition(7, 2); soc.setSize(2, 2); soc.makePanel(); soc.setItemProperties();
-//soc.create();
-//save();
-
-var fb = new LLShortcut(); fb.setName("fb_test"); fb.setLabel("FBx"); fb.setPosition(7, 2); fb.setPkgLabel("Facebook"); //fb.setPkg(""); //fb.getInfo();
-//var fb = new LLShortcut(); fb.setContainer(soc.getContainer()); fb.setName("fb_test"); fb.setLabel("FBx"); fb.setPosition(0, 0); fb.setPkgLabel("Facebook"); //fb.setPkg(""); //fb.getInfo();
-fb.setLabelSize(30); fb.setProperties();//fb.create();
-
-//var sc = new LLShortcut(); sc.setContainer(soc.getContainer()); sc.setName("sc_test"); sc.setLabel("SCx"); sc.setPosition(0, 1); sc.setPkgLabel("Snapchat"); //sc.setPkg(""); //sc.getInfo();
-//sc.setLabelSize(20); sc.create();
-
-//var fbb = fb; fbb.setPosition(8, 2); fbb.create(); //not creating another shortcut 
-
-
-save();
-/*//restart the launcher
-bindClass("java.lang.System");
-System.runFinalization(); System.exit(0);
-*/
-
-/*
 function draw_circle(itm){
 	var drawing = {
 		draw: function(context) {
@@ -719,4 +872,35 @@ gentle alarm     - com.mobitobi.android.gentlealarm
 Popup Widget     - com.ss.popupWidget
 Tasker             - net.dinglisch.android.taskerm
 Zooper Widget     - org.zooper.zwpro
+*/
+
+
+//########################
+//########################
+//########################
+// find item > name, id, container
+//check container, from current desktop recursive skip container, check all desktops recursive skip prev desktop
+//check drawer
+//if not found, ask to create it
+//return array of items
+//will help at the creation, with badges positioning, removal
+
+//var soc = new LLPanel(); soc.setName("social_test"); soc.setPosition(7, 2); soc.setSize(2, 2); soc.makePanel(); soc.setItemProperties();
+//soc.create();
+//save();
+
+var fb = new LLShortcut(); fb.setName("fb_test"); fb.setLabel("FBx"); fb.setPosition(7, 2); fb.setPkgLabel("Facebook"); //fb.setPkg(""); //fb.getInfo();
+//var fb = new LLShortcut(); fb.setContainer(soc.getContainer()); fb.setName("fb_test"); fb.setLabel("FBx"); fb.setPosition(0, 0); fb.setPkgLabel("Facebook"); //fb.setPkg(""); //fb.getInfo();
+fb.setLabelSize(30); fb.setProperties();//fb.create();
+
+//var sc = new LLShortcut(); sc.setContainer(soc.getContainer()); sc.setName("sc_test"); sc.setLabel("SCx"); sc.setPosition(0, 1); sc.setPkgLabel("Snapchat"); //sc.setPkg(""); //sc.getInfo();
+//sc.setLabelSize(20); sc.create();
+
+//var fbb = fb; fbb.setPosition(8, 2); fbb.create(); //not creating another shortcut 
+
+
+save();
+/*//restart the launcher
+bindClass("java.lang.System");
+System.runFinalization(); System.exit(0);
 */
