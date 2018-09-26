59d1d611bd93524f14f6e2b2b72c937c3bdb7d9f
jorgepr13 <jorgepr13@gmail.com>
2018-09-25 19:47:26 -0700

    modified variable editor, var typo error

--- Variable_editor.js
+++ Variable_editor.js
@@ -7,8 +7,8 @@ bindClass("java.io.File");
 
 var LLvars, LLlist;
 var title = "Variable Editor";
-var msg = new dialogMessage();    msg.setId("msg");   msg.setTitleText(title); msg.setButtonPositiveText("OK"); msg.setButtonNegativeText("Cancel"); 
-var chk = new dialogCheckbox();   chk.setId("check"); chk.setTitleText(title); chk.setButtonNeutralText("Select All"); 
+var msg = new dialogMessage();    msg.setId("msg");   msg.setTitleText(title); msg.setButtonPositiveText("OK"); msg.setButtonNegativeText("Cancel");
+var chk = new dialogCheckbox();   chk.setId("check"); chk.setTitleText(title); chk.setButtonNeutralText("Select All");
 var txt = new dialogTextInput(2); txt.setId("text");  txt.setTitleText(title); txt.setLabelText(["Name", "Value"]); txt.setHintText(["Name", "Value"]); txt.setButtonPositiveText("Save"); txt.setButtonNegativeText("Close"); txt.setButtonNeutralText("Delete"); txt.stayOnClickButtonPositive();
 var mnu = new dialogList();       mnu.setId("menu");  mnu.setTitleText(title); mnu.exitOnClick(); mnu.setButtonPositiveText("Add"); mnu.setButtonNegativeText("Exit"); mnu.setButtonNeutralText("Delete Menu");
 getLLVars();
@@ -29,14 +29,14 @@ if (myDialog.id == mnu.getId()) {
   //if (myDialog.button == Dialog.BUTTON_NEGATIVE) {try {if (receiver != null) {context.unregisterReceiver(receiver);}} catch (e) {alert(e.toString());}} else //exit
   if (myDialog.button == Dialog.BUTTON_POSITIVE) {txt.hideButtonNeutral(); txt.setInputText(["", ""]); txt.show(); txt.showButtonNeutral();} else //create var
   if (myDialog.button == Dialog.BUTTON_NEUTRAL) {chk.show();} //delete menu
-} else 
+} else
 
 if (myDialog.id == txt.getId()) {
   if (!emptyVariable(myDialog.text[0])) {var name = myDialog.text[0]; name = name.trim(); name = name.replace(/\s+/gm, "_"); myDialog.text[0] = name;}
-  if (myDialog.button == Dialog.BUTTON_NEGATIVE) {mnu.show();} else 
+  if (myDialog.button == Dialog.BUTTON_NEGATIVE) {mnu.show();} else
   if (myDialog.button == Dialog.BUTTON_POSITIVE) {if (!emptyVariable(myDialog.text[0])) {msg.setMessage("\nDo you want to SAVE '" + myDialog.text[0] + "'?\n"); msg.show();}} else //save
   if (myDialog.button == Dialog.BUTTON_NEUTRAL) {msg.setMessage("\nDo you want to DELETE '" + myDialog.text[0] + "'?\n"); msg.show();} //delete
-} else 
+} else
 
 if (myDialog.id == chk.getId()) {
   if (myDialog.button == Dialog.BUTTON_NEGATIVE) {mnu.show();} else //close
@@ -51,7 +51,7 @@ if (myDialog.id == chk.getId()) {
     for (var i = 0; i < myDialog.state.length; i++) {itemsState.push(state);}
     chk.setItemsState(itemsState); chk.show();
   }
-} else 
+} else
 
 if (myDialog.id == msg.getId()) {
   if (myDialog.button == Dialog.BUTTON_POSITIVE) {
@@ -63,8 +63,8 @@ if (myDialog.id == msg.getId()) {
       if (prevDialog.button == Dialog.BUTTON_POSITIVE) {for (var i = 0; i < prevDialog.state.length; i++) {if (prevDialog.state[i]) {deleteLLVar(LLvars.v[i].n);}}  }
     }
   }
-  
-  if (prevDialog.id == chk.getId()) {chk.show();} else 
+
+  if (prevDialog.id == chk.getId()) {chk.show();} else
   if (prevDialog.id == txt.getId() && prevDialog.button == Dialog.BUTTON_NEUTRAL) {mnu.show();}
 }
 
@@ -76,18 +76,18 @@ function getLLVars() {
   LLvars = JSON.parse(read("data/data/net.pierrox.lightning_launcher_extreme/files/variables"));//reads the content of the file
   //var result = (condition) ? (value1) : (value2) ;
   //LLlist = LLvars.v == null?[]:LLvars.v.map(function(a){var txt = a.v; if (txt.length > 100) {txt = txt.substring(0, 100) + " ...";} return a.n + ": '" + txt + "'";}); //extracts the variable names, if any
-  if (LLvars.v == null) {LLlist = [];} 
+  if (LLvars.v == null) {LLlist = [];}
   else {LLlist = LLvars.v.map(function(a) {
     var txt = a.v; if (txt.length > 100) {txt = txt.substring(0, 100) + " ...";}
     return a.n + ": '" + txt + "'";
   });}
-  mnu.setItems(LLlist); chk.setItems(LLlist); 
+  mnu.setItems(LLlist); chk.setItems(LLlist);
 }
 function deleteLLVar(name) { //showToast("Delete:\n" + name );
   if (!emptyVariable(name)) {name = name.trim(); name = name.replace(/\s+/gm, "_"); getVariables().edit().setString(name, null).commit();}
   getLLVars();
 }
-function setLLVar(name, value) { 
+function setLLVar(name, value) {
   if (emptyVariable(name)) {return;}
   if (!emptyVariable(value)) {value = value.trim();}
   name = name.trim(); name = name.replace(/\s+/gm, "_");
@@ -103,10 +103,10 @@ function setLLVar(name, value) {
 }
 
 function varTypeVal(str) {
-  //if (str == "null") {return null;} 
+  //if (str == "null") {return null;}
   if (!str) {return ["string", ""];}
-  var match; srt = str.toString();
-  match = srt.match(/^true$|^false$/); if (match) {return ["boolean", (match[0] == "true")];}
+  var match; str = str.toString();
+  match = str.match(/^true$|^false$/); if (match) {return ["boolean", (match[0] == "true")];}
   match = str.match(/^-?\d+\.\d+$/);   if (match) {return ["float",   Number(match[0])];}
   match = str.match(/^-?\d+$/);        if (match) {return ["int",     Number(match[0])];}
   return ["string", str];
