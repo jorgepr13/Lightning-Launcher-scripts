1625f48d4289abf8c9bbcffcbd9f34b260b391d0
jorgepr13 <jorgepr13@gmail.com>
2018-09-25 19:08:22 -0700

    started working with categories

--- Categories.js
+++ Categories.js
@@ -6,7 +6,7 @@
 
 
 //shortcuts can be added manually while adding them to a category
-//during the dulicate process, the newly duplicated item id will be added to the id list, 
+//during the dulicate process, the newly duplicated item id will be added to the id list,
 //if it don't have a pkg or if it's a shortcut
 
 /*
@@ -21,12 +21,17 @@ adding an item to a category
 
 -get the containers for the categories selected
 --check if the container is repeated/have multi categories by matching the id
---get all the apps for the categories 
+--get all the apps for the categories
 --get all the items in the container
 
 
 
 */
+
+/*
+var main = {};
+main.categories = [];
+
 
 var catt = []; //[{name,container,app},{...}]
 var cat = catt[0];
@@ -46,7 +51,6 @@ apps.name = "";
 apps.id = "";
 
 
-
 cont.name = "";
 cont.id = "";
 cont.cat = [];
@@ -59,4 +63,86 @@ apps = []; //[{id, lbl, pkg}, {...}]
 apps.id = [];
 apps.lbl = "";
 apps.pkg = "";
+*/
+
+
+
+//main = {categories[], containers[]}
+//categories = [{name,container[{name, id}],app[{name, id}]}]
+
+
+var categories = []; //[{name,container,apps},{...}]
+categories[0] = {};
+var cat = categories[0];
+cat.name = "";
+cat.cont = []; //[{name, id},{...}]
+cat.apps = []; //[{name, id},{...}]
+cat.cont[0] = {};
+cat.apps[0] = {};
+
+var cont = cat.cont[0];
+cont.name = "";
+cont.id = "";
+
+var apps = cat.apps[0];
+apps.name = "";
+apps.id = "";
+
+
+
+
+var inCat = [];
+var inApp = [];
+//add app
+//given categories, given app
+for (i = 0; i < categories.length; i++) {
+  for (j = 0; j < inCat.length; j++) {
+    if (categories[i] == inCat[j]) {
+      //some code
+      for (m = 0; m < categories[i].apps.length; n++) {
+        for (n = 0; n < inApp.length; n++) {
+          if (categories[i].apps[m].id == inApp[n].id) {
+            //some code
+          }
+        }//inApp
+      }//apps
+    }//categories match
+  }//inCat
+}//categories
+
+
+
+
+
+
+
+
+//samples
+
+json_obj = JSON.parse(json_str);
+var wx_str = JSON.stringify(categories, null, '  ');
+
+function get_itm_icon(elem) {
+  if (elem == null) {return;} //no element received
+  if (json_str == null || json_str == "") {return;} //no file contents
+  if (typeof json_obj.fonts.font[0] == "undefined") {return;} //no object
+
+  var data = {};
+  data.path = "";
+  data.icon = "";
+  for (i = 0; i < json_obj.fonts.font.length; i++) {
+    for (j = 0; j < json_obj.fonts.font[i].element.length; j++) {
+      if (json_obj.fonts.font[i].element[j].name.indexOf(elem) != -1) {
+        data.path = json_obj.fonts.font[i].path;
+        data.icon = json_obj.fonts.font[i].element[j].icon;
+        break;
+      }
+    }
+  }
+  data = JSON.stringify(data);
+  if (typeof data == "undefined" || data == null)  {return;}
+  else {return data;}
+}
+
+
 
